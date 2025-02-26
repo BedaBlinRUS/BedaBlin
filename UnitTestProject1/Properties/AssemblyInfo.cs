d799ec324de4f2d6e6d0d0571a8f335ade42e15b
BedaBlinRUS <luiche793@gmail.com>
2025-02-26 10:35:12 +0300

    Add files via upload

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("50ff14b0-9d6b-4234-b32d-9ded357b347c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RectangleAreact1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+namespace MathTaskClassLibraryTests
+    {
+        [TestClass]
+        public class GeometryTests
+        {
+            [TestMethod]
+            public void RectangleArea_3and5_15returned()
+            {
+                // Исходные данные
+                int a = 3;
+                int b = 5;
+                int expected = 15;
+
+                // Получение значения с помощью тестируемого метода
+                Geometry g = new Geometry();
+                int actual = g.RectangleArea(a, b);
+
+                // Сравнение ожидаемого результата с полученным
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+
+}
+    }
+}
